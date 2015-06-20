6f822d735f34238566ef186d7c652851eb70c503
mdb <mdb@x42.net>
2015-06-20 20:16:19 +0200

    Proper exception handling scope

--- Program.cs
+++ Program.cs
@@ -10,38 +10,41 @@ namespace ArchiveTool
 
         public static void Main(string[] args)
         {
+            CommandLine.ParserResult<ArchiveTool.CommandLineOptions> result = null;
+
             try
             {
-                var result = CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args);
-                if (!result.Errors.Any())
-                {
-                    string outputPath = result.Value.OutputPath;
-                    if (string.IsNullOrEmpty(outputPath))
-                        outputPath = Environment.CurrentDirectory;
-
-                    KeyParser explicitKey;
-                    if (!string.IsNullOrEmpty(result.Value.KeyFile))
-                        explicitKey = new KeyParser(result.Value.KeyFile);    
-
-                    if (result.Value.ObjectType.ToLower().StartsWith("media"))
-                        WildcardExpander(result.Value.InputFile, file => MediaProcessor.Scan(file, outputPath, result.Value.DoRepair, result.Value.DoExtract, result.Value.Verbose));
-                    else if (result.Value.ObjectType.ToLower().StartsWith("slice"))
-                        WildcardExpander(result.Value.InputFile, file => ArchiveSliceProcessor.Process(file, outputPath, result.Value.DoRepair, result.Value.DoExtract, result.Value.Verbose));
-                    else if (result.Value.ObjectType.ToLower().StartsWith("archive"))
-                        WildcardExpander(result.Value.InputFile, file => ArchiveSetProcessor.Scan(file, explicitKey, outputPath, result.Value.DoExtract, result.Value.Verbose));
-                    else if (result.Value.ObjectType.ToLower().StartsWith("small"))
-                        WildcardExpander(result.Value.InputFile, file => SmallFileBundleProcessor.Scan(file, outputPath, result.Value.DoExtract, result.Value.Verbose));
-                    else
-                    {
-                        Console.WriteLine("Invalid object type: should be Media, Slice, Archive or SmallFileBundle");
-                        Environment.Exit(1);
-                    }
-                }
+                result = CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args);
             }
             catch (Exception)
             {
                 Console.WriteLine("Invalid command line. Use --help to see valid options.");
                 Environment.Exit(1);
+            }
+
+            if (!result.Errors.Any())
+            {
+                string outputPath = result.Value.OutputPath;
+                if (string.IsNullOrEmpty(outputPath))
+                    outputPath = Environment.CurrentDirectory;
+
+                KeyParser explicitKey = null;
+                if (!string.IsNullOrEmpty(result.Value.KeyFile))
+                    explicitKey = new KeyParser(result.Value.KeyFile);
+
+                if (result.Value.ObjectType.ToLower().StartsWith("media"))
+                    WildcardExpander(result.Value.InputFile, file => MediaProcessor.Scan(file, outputPath, result.Value.DoRepair, result.Value.DoExtract, result.Value.Verbose));
+                else if (result.Value.ObjectType.ToLower().StartsWith("slice"))
+                    WildcardExpander(result.Value.InputFile, file => ArchiveSliceProcessor.Process(file, outputPath, result.Value.DoRepair, result.Value.DoExtract, result.Value.Verbose));
+                else if (result.Value.ObjectType.ToLower().StartsWith("archive"))
+                    WildcardExpander(result.Value.InputFile, file => ArchiveSetProcessor.Scan(file, explicitKey, outputPath, result.Value.DoExtract, result.Value.Verbose));
+                else if (result.Value.ObjectType.ToLower().StartsWith("small"))
+                    WildcardExpander(result.Value.InputFile, file => SmallFileBundleProcessor.Scan(file, outputPath, result.Value.DoExtract, result.Value.Verbose));
+                else
+                {
+                    Console.WriteLine("Invalid object type: should be Media, Slice, Archive or SmallFileBundle");
+                    Environment.Exit(1);
+                }
             }
         }
 
