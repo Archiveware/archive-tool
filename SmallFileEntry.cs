b5cf808252b5eead73058eb4d9d1ca793abe2925
Michiel de Bruijn <mdb@x42.net>
2016-05-05 09:21:34 +0200

    Small file entries can now be up to 32MB in size

--- SmallFileEntry.cs
+++ SmallFileEntry.cs
@@ -9,7 +9,7 @@ namespace ArchiveTool
 {
     class SmallFileEntry
     {
-        public UInt16 DataLength;
+        public UInt32 DataLength;
         public DateTime Created;
         public DateTime LastModified;
         public UInt32 DataCrc;
@@ -29,27 +29,27 @@ namespace ArchiveTool
         {
             try
             {
-                byte[] buffer = new byte[132000];
+                byte[] buffer = new byte[33000000];
                 long initialStreamPosition = fs.Position;
                 fs.Read(buffer, 0, buffer.Length);
 
-                DataLength = BitConverter.ToUInt16(buffer, 0);
-                Created = new DateTime(BitConverter.ToInt64(buffer, 2));
-                LastModified = new DateTime(BitConverter.ToInt64(buffer, 10));
-                DataCrc = BitConverter.ToUInt32(buffer, 18);
-                FullNameLength = BitConverter.ToUInt16(buffer, 22);
+                DataLength = BitConverter.ToUInt32(buffer, 0);
+                Created = new DateTime(BitConverter.ToInt64(buffer, 4));
+                LastModified = new DateTime(BitConverter.ToInt64(buffer, 12));
+                DataCrc = BitConverter.ToUInt32(buffer, 20);
+                FullNameLength = BitConverter.ToUInt16(buffer, 24);
                 FullNameUtf8Bytes = new byte[FullNameLength];
-                Array.ConstrainedCopy(buffer, 24, FullNameUtf8Bytes, 0, FullNameLength);
+                Array.ConstrainedCopy(buffer, 26, FullNameUtf8Bytes, 0, FullNameLength);
                 FullName = UTF8Encoding.UTF8.GetString(FullNameUtf8Bytes);
                 Data = new byte[DataLength];
-                Array.ConstrainedCopy(buffer, 24 + FullNameLength, Data, 0, DataLength);
+                Array.ConstrainedCopy(buffer, 26 + FullNameLength, Data, 0, (int)DataLength);
 
                 if (Verbose)
                     Console.WriteLine("  @{0,-16} {1} ({2})", initialStreamPosition, FullName, DataLength);
                 else
                     Console.Write(".");
 
-                fs.Seek(initialStreamPosition + (24 + FullNameLength + DataLength + 1), SeekOrigin.Begin);
+                fs.Seek(initialStreamPosition + (26 + FullNameLength + DataLength + 1), SeekOrigin.Begin);
 
                 if (Crc32CWrapper.ComputeCrc32C(Data, 0, Data.Length) != DataCrc)
                     throw new ApplicationException("CRC mismatch");
